e3cbf79d32c26d03f999ce865818880c11f0402a
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-13 23:03:56 +0900

    달에서 무게 구하기

--- Lesson01_출력/Step04_소수점 맞춰 출력하기/04_01.cpp
+++ Lesson01_출력/Step04_소수점 맞춰 출력하기/04_01.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main() {
     // 변수 선언
     int a = 13;
-  	double b = 0.165;
+    double b = 0.165;
 	
 	cout << fixed;  // 소수점 자리 표현을 고정하겠다는 뜻
 	cout.precision(6);  // 소수점 n째자리까지 표현하겠다는 뜻
