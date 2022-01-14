fc25406c63a7db4bcaf02732f74cb0aebf20b495
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-15 00:29:52 +0900

    문자열 받아 출력

---
+++ Lesson02_입출력/Step03_문자, 문자열 출력/03_02.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>  // string 헤더 포함 -> string 배열 이용
+using namespace std;
+
+int main() {
+
+    string a;
+    cin >> a;
+    cout << a;
+
+    return 0;
+    
+}
