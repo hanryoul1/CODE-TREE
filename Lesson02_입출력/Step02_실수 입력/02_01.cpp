9f6460290a41ebecb8ff010ba9bfca841321f962
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-15 00:10:53 +0900

    실수 받아 그대로 출력

---
+++ Lesson02_입출력/Step02_실수 입력/02_01.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    double n;  // 실수 type인 double 변수
+    cin >> n;
+
+    cout << fixed; 
+	cout.precision(2);
+    cout << n;
+
+    return 0;
+
+}
