fc30ae17191572b517640418e7d1fa2dd5ea46ac
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-15 21:18:44 +0900

    합과 평균

---
+++ Lesson03_연산자/Step02_합과 평균/02_01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b;
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(1);
+    
+    cout << a + b;
+    cout << " ";
+    cout << (double)(a+b) / 2;
+
+    return 0;
+
+}
