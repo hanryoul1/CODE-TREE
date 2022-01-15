c36b4722edb58aca9fc8ecf7dae5e22de05e1e17
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-15 21:43:17 +0900

    체질량지수

---
+++ Lesson04_조건문/Step01_if 조건문/01_03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int h, w;
+    cin >> h >> w;
+
+    int bmi = w * 100 * 100 / (h * h);  // h(cm -> m)
+
+    cout << bmi << endl;
+
+    if (bmi >= 25)
+        cout << "Obesity";
+
+    return 0;
+
+}
