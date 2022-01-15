9abfa4d0e598530f33d85990eafd4c64c1709e19
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-15 21:43:43 +0900

    음수 구별하기

--- Lesson04_조건문/Step01_if 조건문/01_01.cpp
+++ Lesson04_조건문/Step01_if 조건문/01_01.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+
+    int a;
+    cin >> a;
+    cout << a;
+
+    if (a < 0) {
+        cout << endl << "minus"; 
+    }
+
+    return 0;
+
+}
