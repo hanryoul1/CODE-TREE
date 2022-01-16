515a0b97bf71dce991545f6ae1e1572bef8ff705
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-16 12:59:00 +0900

    점수 비교

---
+++ Lesson04_조건문/Step09_and 기호/09_01.cpp
@@ -0,0 +1,21 @@
+// && (and)
+// if (조건1 && 조건2)
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int A1, A2, B1, B2;
+    cin >> A1 >> A2;
+    cin >> B1 >> B2;
+
+    if (A1 > B1 && A2 > B2)
+        cout << 1;
+
+    else
+        cout << 0;
+
+    return 0;
+
+}
