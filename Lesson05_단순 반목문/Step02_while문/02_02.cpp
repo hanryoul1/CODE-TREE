8e6dd5cabb76889806ae49068e2878396031a0ef
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-01-20 18:17:56 +0900

    n까지 숫자 출력

---
+++ Lesson05_단순 반목문/Step02_while문/02_02.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, i;
+    i = 1;
+    cin >> n;
+
+    while (i != n+1) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+
+}
