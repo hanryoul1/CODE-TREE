373831b097656eba1b30c562e87b45c25c1f84c8
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-02-01 14:36:29 +0900

    배수의 개수 2

--- Lesson05_단순 반목문/Step06_cnt 활용하기/06_01.cpp
+++ Lesson05_단순 반목문/Step06_cnt 활용하기/06_01.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+
+    int a, cnt = 0;
+
+    for (int i = 1; i <= 10; i++) {
+        cin >> a;
+
+        if (a % 2 != 0) {
+            cnt++;
+        }
+    }
+
+    cout << cnt;
+    return 0;
+    
+}
