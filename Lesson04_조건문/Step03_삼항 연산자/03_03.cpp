e4778f4fb60f0917f113883540cadfdb799a5ef4
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-06 15:49:16 +0900

    삼항연산자 2

---
+++ Lesson04_조건문/Step03_삼항 연산자/03_03.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    cin >> a;
+
+    if (a == 1)
+        cout << 't';
+
+    else 
+        cout << 'f';
+
+    return 0;
+}
