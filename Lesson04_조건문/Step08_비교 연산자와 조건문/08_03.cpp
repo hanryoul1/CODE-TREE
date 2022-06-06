55f4e90a90e380e5fb04aa8b41e83460fdac6ab4
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-06 16:05:06 +0900

    4번의 크기 비교

---
+++ Lesson04_조건문/Step08_비교 연산자와 조건문/08_03.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a, b, c, d, e;
+    cin >> a >> b >> c >> d >> e;
+
+    if (a > b)
+        cout << "1 ";
+    else
+        cout << "0 ";
+
+    if (a > c)
+        cout << "1 ";
+    else
+        cout << "0 ";
+
+    if (a > d)
+        cout << "1 ";
+    else
+        cout << "0 ";
+
+    if (a > e)
+        cout << "1 ";
+    else
+        cout << "0 ";
+
+    return 0;
+}
