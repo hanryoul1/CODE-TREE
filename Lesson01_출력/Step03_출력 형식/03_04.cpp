c248d23a874e35b1d7e1908af4b5e8edb1bcde58
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-06 14:52:15 +0900

    변수 출력하기 3

---
+++ Lesson01_출력/Step03_출력 형식/03_04.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 변수 선언
+    int a = 1;
+    int b = 2;
+	char c = 'C';
+    
+    // 출력
+    cout << a << "->" << b << "->" << c;
+    return 0;
+}
