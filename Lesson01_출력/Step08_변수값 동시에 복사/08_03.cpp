0885aa576ee836fd047025a70d31fe07bd576c22
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-06 15:07:16 +0900

    합을 복사하기

---
+++ Lesson01_출력/Step08_변수값 동시에 복사/08_03.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int a = 1;
+    int b = 2;
+    int c = 3;
+
+    int d = a + b + c;
+    a = b = c = d;
+
+    cout << a << " " << b << " " << c;
+    return 0;
+}
