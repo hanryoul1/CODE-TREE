8242361e2e78f59e1c0baa12a126484d099829fd
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-06 15:33:30 +0900

    전화번호 바꾸기

---
+++ Lesson02_입출력/Step06_특정 문자를 사이에 두고 입력/06_05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char dash;
+    int a, b, c;
+
+    cin >> a >> dash >> b >> dash >> c;
+    cout << "010" << dash << c << dash << b;
+
+    return 0;
+}
