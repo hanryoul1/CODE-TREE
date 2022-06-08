97c2f2778aaec9c35896a32d1a41b397e544f86f
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-09 00:47:36 +0900

    홀수만 출력

---
+++ Lesson05_단순 반목문/Step01_for문/01_09.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    if (a % 2 != 0) {
+        for (int i = a; i <= b; i += 2) {
+            cout << i << " ";
+        }
+    }
+
+    else {
+        for (int j = a; j <= b; j += 2) {
+            cout << j+1 << " ";
+        }
+    }
+
+    return 0;
+}
