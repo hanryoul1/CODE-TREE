81b747287f037905888bed87672161e4e0c540b7
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-10 00:56:41 +0900

    주어진 수까지의 곱

--- Lesson05_단순 반목문/Step08_prod 계산하기/08_01.cpp
+++ Lesson05_단순 반목문/Step08_prod 계산하기/08_01.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    int prod = 1;
+
+    for (int i = a; i <= b; i++) {
+        prod = prod * i;
+    }
+
+    cout << prod;
+    return 0;
+}   
