6bb279d795785af021cba7b038406aba445d1e53
Han Ryoul <83659452+hanryoul1@users.noreply.github.com>
2022-06-16 12:55:17 +0900

    친근하지 않은 수

--- Lesson05_단순 반목문/Step09_continue문/09_01.cpp
+++ Lesson05_단순 반목문/Step09_continue문/09_01.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 using namespace std;
+
+int main() {
+    
+    int cnt = 0;
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        if (i%2==0||i%3==0||i%5==0) {
+            continue;
+        }
+        cnt++;
+    }
+
+    cout << cnt;
+    return 0;
+}
